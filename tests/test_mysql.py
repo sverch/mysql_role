dae7d2956cc85f0a5cc32c406ba84ab2942f5b0d
dortega <dortega@idealista.com>
2017-09-25 11:26:15 +0200

    #12 first approach

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -13,15 +13,21 @@ def test_mysql_packages(Package):
     assert Package("mysql-server").is_installed
 
 
-def test_mysql_service(File, Service, Socket):
+def test_mysql_service(File, Service, Socket, AnsibleDefaults):
+    mysql_port = AnsibleDefaults["mysql_port"]
+
     assert File("/etc/init.d/mysql")
     assert Service("mysql").is_enabled
     assert Service("mysql").is_running
-    assert Socket("tcp://127.0.0.1:3306")
+    assert Socket("tcp://127.0.0.1:" + str(mysql_port))
 
 
-def test_mysql_database(Command, AnsibleDefaults):
+def test_mysql_database(Command):
     BD = "echo \'show databases;\'|mysql -u root -pdefault|grep -w test01"
     User = "echo \'show databases;\'|mysql -u testing -ptesting|grep -w test01"
     assert Command(BD).rc is 0
     assert Command(User).rc is 0
+
+
+def test_config(Command):
+    assert Command("sudo mysql --defaults-extra-file=/etc/mysql/my.cnf -sN -e \'SHOW VARIABLES WHERE variable_name=\"version_comment\"\'").stdout is "version_comment	MySQL Community Server (GPL)"
